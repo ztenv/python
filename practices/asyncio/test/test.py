784559136307a4909b7564b8239d764bfa85794b
shlian <shaohua.lian@xuncetech.com>
2018-12-15 09:05:18 +0800

    add asyncio test

---
+++ practices/asyncio/test/test.py
@@ -0,0 +1,15 @@
+import asyncio
+
+async def hello_word(time):
+    print("hello :{}!".format(time))
+    await asyncio.sleep(time)
+    print("hello again:{}".format(time))
+
+
+if __name__=="__main__":
+    loop=asyncio.get_event_loop()
+    tasks=[hello_word(1) for i in range(5)]
+    tasks.append(loop.create_task(hello_word(2)))
+    #loop.run_until_complete(loop.create_task(hello_word(2)))
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
