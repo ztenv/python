6e1413bb3df2604e585e9f144ff8248a17abee3c
shlian <shaohua.lian@xuncetech.com>
2018-12-21 10:09:38 +0800

    add log and config.txt

---
+++ practices/picture/resize/log.py
@@ -0,0 +1,13 @@
+import logging
+import yaml
+import logging.config
+import os
+
+def setup(default_path='config.yaml', default_level=logging.INFO):
+    path = default_path
+    if os.path.exists(path):
+        with open(path, 'r', encoding='utf-8') as f:
+            config = yaml.load(f)
+            logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
--- practices/picture/resize/main.py
+++ practices/picture/resize/main.py
@@ -2,14 +2,28 @@ import sys
 import os
 import traceback
 from PIL import Image
+import log
+import logging
+import json
+logger=logging.getLogger("main")
 
 class image_resizer(object):
-    def __init__(self,base_path="./pics"):
-        self._base_path=base_path
-        self._pictures=[".jpg",".jpeg",".png",".bmp",".tif"]
+    def __init__(self):
+        logger.info("work dir:{0}".format(os.getcwd()))
+        self._base_path_list=[]
+        self._pictures=[]
+        self._load_config()
+
+    def _load_config(self):
+        with open("config.txt","r") as f:
+            load_dict=json.load(f)
+            for item in load_dict["path"]:
+                self._base_path_list.append(os.path.join(os.getcwd(),item))
+            for ext in load_dict["ext"]:
+                self._pictures.append(ext)
 
     def _find_all_pictures(self,base_path):
-        print("base_path:{0}".format(base_path))
+        logger.info("sub_scan_path:{0}".format(base_path))
         for root,dirs,files in os.walk(base_path):
             for dirname in dirs:
                 self._find_all_pictures(dirname)
@@ -23,44 +37,33 @@ class image_resizer(object):
     def _resize_picture(self,file_name):
         image= Image.open(file_name)
         w,h=image.size
-        print("width={0},height={1}".format(w,h))
+        logger.info("pic_name:{0},resolution({1},{2})".format(file_name,w,h))
         scale=1024/w if(1024/w>1024/h) else 1024/h
-        w=w*scale
-        h=h*scale
+        w=int(w*scale)
+        h=int(h*scale)
         if(w>=1024)and(h>=1024):
-            print("new_width={0},new_height={1}".format(w,h))
             new_image=image.resize((int(w),int(h)))
             new_image=new_image.convert('RGB')
-            new_image.save("{0}-new{1}".format(file_name[:file_name.rfind(".")],file_name[file_name.rfind("."):]))
+            new_file_name="{0}-shlian_pengjian{1}".format(file_name[:file_name.rfind(".")],file_name[file_name.rfind("."):])
+            logger.info("save to:{0},resolution({1},{2})".format(new_file_name,w,h))
+            new_image.save(new_file_name)
             new_image.close()
         image.close()
 
     def resize_all(self):
-        base_path=os.getcwd()
-        for item in self._find_all_pictures(base_path):
-            print(item)
-            try:
-                self._resize_picture(item)
-            except Exception as ee:
-                print("ErrorMessage:{0}".format(ee))
-                print(traceback.format_exc())
+        for path in self._base_path_list:
+            for item in self._find_all_pictures(path):
+                try:
+                    self._resize_picture(item)
+                except Exception as ee:
+                    logger.error("resize:{0} error.".format(item))
+                    logger.error("ErrorMessage:{0}".format(ee))
+                    logger.error(traceback.format_exc())
+            logger.info("***********")
 
 if __name__=="__main__":
+    log.setup("logconfig.yaml")
     ir=image_resizer()
     ir.resize_all()
-
-#if __name__=="__main__":
-#    image_file=sys.argv[1]
-#    image=Image.open(image_file)
-#    w,h=image.size
-#    print("width={0},height={1}".format(w,h))
-#    scale=1024/w if(1024/w>1024/h) else 1024/h
-#    print("scale={0}".format(scale))
-#    w=w*scale
-#    h=h*scale
-#    if(w>=1024)and(h>=1024):
-#        print("new_width={0},new_height={1}".format(w,h))
-#        new_image=image.resize((int(w),int(h)))
-#        new_image.save("new-{}".format(image_file))
-#        new_image.close()
-#    image.close()
+    print("press enter key to exit")
+    sys.stdin.readline()
