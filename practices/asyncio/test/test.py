90d8a818a23088f28c10634fe3b9658183f4ad07
shlian <shaohua.lian@xuncetech.com>
2018-12-21 10:14:07 +0800

    modify test.py

--- practices/asyncio/test/test.py
+++ practices/asyncio/test/test.py
@@ -8,6 +8,7 @@ async def hello_word(time):
 
 if __name__=="__main__":
     loop=asyncio.get_event_loop()
+
     tasks=[hello_word(1) for i in range(5)]
     tasks.append(loop.create_task(hello_word(2)))
     #loop.run_until_complete(loop.create_task(hello_word(2)))
