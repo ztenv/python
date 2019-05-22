abcfe2da7654775d702b73a2c3e31f788af46e07
shlian <shaohua.lian@xuncetech.com>
2019-05-22 14:57:23 +0800

    add readme.txt and test_command2.py

---
+++ project/mms/webserver/kline/management/commands/test_command2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @file   : test_command2.py
+# @author : shlian
+# @date   : 2019/5/22
+# @version: 1.0
+# @desc   :
+import datetime
+import time
+from django.core.management.base import BaseCommand,CommandError
+class Command(BaseCommand):
+    def handle(self,*args,**options):
+        while(True):
+            self.stdout.write("test2....{0}".format(datetime.datetime.now().timestamp()))
+            time.sleep(1)
