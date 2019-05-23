9376bb0416b9fcefc45324cb356d8cad760c58ff
shlian <shaohua.lian@xuncetech.com>
2019-05-23 18:07:22 +0800

    mod:修改类型

--- project/mms/webserver/kline/biz/table_factory.py
+++ project/mms/webserver/kline/biz/table_factory.py
@@ -6,24 +6,24 @@
 # @desc   :
 from kline.biz.sub_models import huobi_kline,huobi_one_min_kline
 
-
-
-
-
-
-
-
-
-
-
-
-
+#############################
+#0	binance
+#1	huobi
+#2	poloniex
+#3	okex
+#4	bitfinex
+#5	gate.io
+#6	bibox
+#7	bittrex
+#8	bitstamp
+#9	bitMax
+#10	kraken
 
 class table_factory(object):
     def __init__(self):
         self._table={
-            '1':(huobi_one_min_kline,huobi_kline)
+            1:(huobi_one_min_kline,huobi_kline)
         }
 
     def get(self,exchange_id):
-        return self._table.get(exchange_id,None)
+        return self._table.get(int(exchange_id),None)
