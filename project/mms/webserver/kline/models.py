4ed86351c04245cddea77f5657c6d299ceed1f2a
shlian <shaohua.lian@xuncetech.com>
2019-05-29 18:28:45 +0800

    mod:1、mysql scripts add index;2、modify models

--- project/mms/webserver/kline/models.py
+++ project/mms/webserver/kline/models.py
@@ -4,8 +4,8 @@ from django.db import models
 from django.db import models
 
 class one_min_kline(models.Model):
-    contract_id=models.IntegerField(db_column='contract_id',blank=False)
-    timestamp=models.BigIntegerField(db_column='timestamp',blank=False)
+    contract_id=models.IntegerField(db_column='contract_id',blank=False,db_index=True)
+    timestamp=models.BigIntegerField(db_column='timestamp',blank=False,db_index=True)
     high=models.DecimalField(db_column='high_price',max_length=20,max_digits=20,decimal_places=8,blank=False,default=-1)
     open=models.DecimalField(db_column='open_price',max_length=20,max_digits=20,decimal_places=8,blank=False,default=-1)
     low=models.DecimalField(db_column='low_price',max_length=20,max_digits=20,decimal_places=8,blank=False,default=-1)
@@ -15,10 +15,11 @@ class one_min_kline(models.Model):
         abstract=True
         unique_together=('contract_id','timestamp')
 
+
 class kline(models.Model):
-    contract_id=models.IntegerField(db_column='contract_id',blank=False)
-    kline_type=models.IntegerField(db_column='kline_type')
-    timestamp=models.BigIntegerField(db_column='timestamp',blank=False)
+    contract_id=models.IntegerField(db_column='contract_id',blank=False,db_index=True)
+    kline_type=models.IntegerField(db_column='kline_type',db_index=True)
+    timestamp=models.BigIntegerField(db_column='timestamp',blank=False,db_index=True)
     high=models.DecimalField(db_column='high_price',max_length=20,max_digits=20,decimal_places=8,blank=False,default=-1)
     open=models.DecimalField(db_column='open_price',max_length=20,max_digits=20,decimal_places=8,blank=False,default=-1)
     low=models.DecimalField(db_column='low_price',max_length=20,max_digits=20,decimal_places=8,blank=False,default=-1)
