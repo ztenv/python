5334fe4bb6e983dfc4651b626dba93eaf69b2a96
shlian <shaohua.lian@xuncetech.com>
2019-05-29 18:38:03 +0800

    fix:default db engine

--- project/mms/webserver/scripts/zc_mms.sql
+++ project/mms/webserver/scripts/zc_mms.sql
@@ -12,7 +12,7 @@ MySQL - 8.0.16 : Database - zc_mms
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`zc_mms` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`zc_mms` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 USE `zc_mms`;
 
@@ -34,7 +34,7 @@ CREATE TABLE `huobi_1m_kline` (
   KEY `contract_id_index` (`contract_id`),
   KEY `timestamp_index` (`timestamp`),
   KEY `id_index` (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `huobi_1m_kline` */
 
@@ -64,7 +64,7 @@ CREATE TABLE `huobi_kline` (
   KEY `timestamp_index` (`timestamp`),
   KEY `id_index` (`id`),
   KEY `kline_type_index` (`kline_type`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 /*Data for the table `huobi_kline` */
 
