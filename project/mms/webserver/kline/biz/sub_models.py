9cf490b55fdf55dc52d0cc31df0d4858d305b56c
shlian <shaohua.lian@xuncetech.com>
2019-06-04 11:28:47 +0800

    add:增加所有交易所的历史K线表

--- project/mms/webserver/kline/biz/sub_models.py
+++ project/mms/webserver/kline/biz/sub_models.py
@@ -49,3 +49,52 @@ class okex_kline(kline):
     class Meta(kline.Meta):
         db_table='okex_kline'
 
+class bitfinex_one_min_kline(one_min_kline):
+    class Meta(one_min_kline.Meta):
+        db_table='bitfinex_1m_kline'
+class bitfinex_kline(kline):
+    class Meta(kline.Meta):
+        db_table='bitfinex_kline'
+
+class gateio_one_min_kline(one_min_kline):
+    class Meta(one_min_kline.Meta):
+        db_table='gate.io_1m_kline'
+class gateio_kline(kline):
+    class Meta(kline.Meta):
+        db_table='gate.io_kline'
+
+class bibox_one_min_kline(one_min_kline):
+    class Meta(one_min_kline.Meta):
+        db_table='bibox_1m_kline'
+class bibox_kline(kline):
+    class Meta(kline.Meta):
+        db_table='bibox_kline'
+
+class bittrex_one_min_kline(one_min_kline):
+    class Meta(one_min_kline.Meta):
+        db_table='bittrex_1m_kline'
+class bittrex_kline(kline):
+    class Meta(kline.Meta):
+        db_table='bittrex_kline'
+
+class bitstamp_one_min_kline(one_min_kline):
+    class Meta(one_min_kline.Meta):
+        db_table='bitstamp_1m_kline'
+class bitstamp_kline(kline):
+    class Meta(kline.Meta):
+        db_table='bitstamp_kline'
+
+class bitMax_one_min_kline(one_min_kline):
+    class Meta(one_min_kline.Meta):
+        db_table='bitMax_1m_kline'
+class bitMax_kline(kline):
+    class Meta(kline.Meta):
+        db_table='bitMax_kline'
+
+class kraken_one_min_kline(one_min_kline):
+    class Meta(one_min_kline.Meta):
+        db_table='kraken_1m_kline'
+class kraken_kline(kline):
+    class Meta(kline.Meta):
+        db_table='kraken_kline'
+
--- project/mms/webserver/kline/biz/table_factory.py
+++ project/mms/webserver/kline/biz/table_factory.py
@@ -24,15 +24,15 @@ class table_factory(object):
         self._table={
             0:(binance_one_min_kline,binance_kline),
             1:(huobi_one_min_kline,huobi_kline),
-            #2:(poloniex_one_min_kline,poloniex_kline),
+            2:(poloniex_one_min_kline,poloniex_kline),
             3:(okex_one_min_kline,okex_kline),
-            #4:(bitfinex_one_min_kline,bitfinex_kline),
-            #5:(gate.io_one_min_kline,gate.io_kline),
-            #6:(bibox_one_min_kline,bibox_kline),
-            #7:(bittrex_one_min_kline,bittrex_kline),
-            #8:(bitstamp_one_min_kline,bitstamp_kline),
-            #9:(bitMax_one_min_kline,bitMax_kline),
-            #10:(kraken_one_min_kline,kraken_kline),
+            4:(bitfinex_one_min_kline,bitfinex_kline),
+            5:(gateio_one_min_kline,gateio_kline),
+            6:(bibox_one_min_kline,bibox_kline),
+            7:(bittrex_one_min_kline,bittrex_kline),
+            8:(bitstamp_one_min_kline,bitstamp_kline),
+            9:(bitMax_one_min_kline,bitMax_kline),
+            10:(kraken_one_min_kline,kraken_kline),
         }
 
     def get(self,exchange_id):
